08968bc60e33944fae8f2698537e790b5bdd583a
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2016-03-14 15:27:30 -0700

    .env-ification

--- src/settings.local.php
+++ src/settings.local.php
@@ -96,13 +96,4 @@ $settings['rebuild_access'] = TRUE;
 
 $settings['hash_salt'] = "mmm...salty";
 
-$databases['default']['default'] = [
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'host' => 'localhost',
-  'port' => '3306',
-  'driver' => 'mysql',
-  'prefix' => '',
-  'collation' => 'utf8mb4_general_ci',
-];
+
